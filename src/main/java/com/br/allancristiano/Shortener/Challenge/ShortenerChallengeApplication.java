7585126675cced1fc97f912d744229fd6b1ffb8c
AllanCristiano <91546885+AllanCristiano@users.noreply.github.com>
2024-06-10 19:01:53 -0300

    first commit
    add initial spring boot

---
+++ src/main/java/com/br/allancristiano/Shortener/Challenge/ShortenerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.br.allancristiano.Shortener.Challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/allancristiano/Shortener/Challenge/ShortenerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.allancristiano.Shortener.Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
